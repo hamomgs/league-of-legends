9c6c19b5882948b13180ab86febb10e734efdcba
hamomgs <hamomgomer947@gmail.com>
2022-07-08 21:16:37 -0300

    responsive layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-const userInput = document.querySelector('#user')
-const passwordInput = document.querySelector('#password')
+const inputs = document.querySelectorAll('input')
 const loginBtn = document.querySelector('button')
-
+const eyeBtn = document.querySelectorAll('.fa-light')
+let [userInput, passwordInput] = inputs
 
 function handleChange() {
   if (userInput.value && passwordInput.value.length >= 8) {
@@ -12,5 +12,21 @@ function handleChange() {
   }
 }
 
-userInput.addEventListener('input', handleChange)
-passwordInput.addEventListener('input', handleChange)
+function showPassword() {
+  let [eye, slashEye] = eyeBtn
+  
+  passwordInput.type == 'password' ? 
+  passwordInput.type = 'text' :
+  passwordInput.type = 'password'
+
+  eye.classList.toggle('invisible')
+  slashEye.classList.toggle('invisible')
+}
+
+inputs.forEach(input => {
+  input.addEventListener('input', handleChange)
+})
+
+eyeBtn.forEach(eye => {
+  eye.addEventListener('click', showPassword)
+})
